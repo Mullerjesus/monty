743cd9d92e60bef3fd239d3c3ba6150566c7527d
Muluken <mulukeneshetu70@gmail.com>
2023-12-14 14:25:34 -0800

    C - Stacks, Queues - LIFO, FIFO

--- bytecodes/001.m
+++ bytecodes/001.m
@@ -1,13 +1,15 @@
-push 0    ; Push 0 onto the stack
-push 1    ; Push 1 onto the stack
-push 2    ; Push 2 onto the stack
-push 3    ; Push 3 onto the stack
-pall      ; Print the entire stack
-
-push hola ; Push 'hola' onto the stack (assuming 'hola' is a string)
-
-push 5    ; Push 5 onto the stack
-push 6    ; Push 6 onto the stack
-pall      ; Print the entire stack
-
-; This is the end of our program. Monty is awesome!
+push 0 Push 0 onto the stack$
+push 1 Push 1 onto the stack$
+$
+push 2$
+  push 3$
+                   pall    $
+$
+$
+                           $
+push 4$
+$
+    push 5    $
+      push    6        $
+$
+pall This is the end of our program. Monty is awesome!$
